8434f099035880af9b41c50104a098fb96b604a6
Nathalie Surman <onepiecenat@hotmail.com>
2023-03-27 15:35:32 -0400

    Fixed problem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 
