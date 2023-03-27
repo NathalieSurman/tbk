c866aa572b927d65af086887b24981f36d6bf0b1
Fennie Tang <fennie1996@gmail.com>
2023-03-27 16:50:41 -0400

    navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { TiTimes } from "react-icons/ti";
 
 export const NavBar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <div>NavBar</div>
-  )
-}
+    <>
+      <nav className="navbar">
+        <div className=" navbar-container">
+          <Link to="/" className="navbar-logo">
+            Kayak
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? <TiTimes /> : <GiHamburgerMenu />} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
