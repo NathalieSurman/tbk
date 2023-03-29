772b6ce6e933ef58ba83fcffe155580237bb7122
Nathalie Surman <onepiecenat@hotmail.com>
2023-03-29 13:35:58 -0400

    Edits to fixs nav bar and added a page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -27,50 +27,60 @@ export const NavBar = () => {
   window.addEventListener("resize", showButton);
   return (
     <>
-      <nav className="navbar">
-        <div className=" navbar-container">
-          <Link to="/" className="navbar-logo">
-            Kayak
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            TRVL
+            <i class='fab fa-typo3' />
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className="nav-item">
+            <li className='nav-item'>
               <Link
-                to="/services"
-                className="nav-links"
+                to='/services'
+                className='nav-links'
                 onClick={closeMobileMenu}
               >
                 Services
               </Link>
             </li>
-            <li className="nav-item">
+            <li className='nav-item'>
               <Link
-                to="/products"
-                className="nav-links"
+                to='/products'
+                className='nav-links'
                 onClick={closeMobileMenu}
               >
                 Products
               </Link>
             </li>
-            <li className="nav-item">
+
+            <li className='nav-item'>
               <Link
-                to="/book-now"
-                className="nav-links-mobile"
+                to='/about-us'
+                className='nav-links'
                 onClick={closeMobileMenu}
               >
-                Sign up
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
               </Link>
             </li>
           </ul>
-          {/* If its true */}
-          {button && <Button buttonStyle="btn--outline">BOOK NOW</Button>}
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
     </>
