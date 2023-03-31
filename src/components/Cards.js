f173cf1d56e4bdcec668efaf6734dafcb424f57a
Fennie Tang <fennie1996@gmail.com>
2023-03-31 12:34:59 -0400

    made changes to packages and options

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,46 +4,50 @@ import "./Cards.css";
 
 export const Cards = () => {
   return (
-    <div className='cards'>
-    <h1>Check out these EPIC Destinations!</h1>
-    <div className='cards__container'>
-      <div className='cards__wrapper'>
-        <ul className='cards__items'>
-          <CardItem
-            src='images/img-9.jpg'
-            text='Explore the hidden waterfall deep inside the Amazon Jungle'
-            label='Adventure'
-            path='/services'
-          />
-          <CardItem
-            src='images/img-2.jpg'
-            text='Travel through the Islands of Bali in a Private Cruise'
-            label='Luxury'
-            path='/services'
-          />
-        </ul>
-        <ul className='cards__items'>
-          <CardItem
-            src='images/img-3.jpg'
-            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-            label='Mystery'
-            path='/services'
-          />
-          <CardItem
-            src='images/img-4.jpg'
-            text='Experience Football on Top of the Himilayan Mountains'
-            label='Adventure'
-            path='/products'
-          />
-          <CardItem
-            src='images/img-8.jpg'
-            text='Ride through the Sahara Desert on a guided camel tour'
-            label='Adrenaline'
-            path='/sign-up'
-          />
-        </ul>
+    <div className="cards">
+      <h1>Packages and Options</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/kayakicon.png"
+              text="Kayak Rentals"
+              // path="/services"
+              subTitle="Single Kayak Rentals"
+           >
+              <div className="cards_items">
+                <h4>Single Kayak Rentals </h4>
+                <ul>
+                  <li>First hour $35</li>
+                  <li>Two hours $55</li>
+                  <li>Weekday daily max $90</li>
+                </ul>
+                <h4>Tandem (Double) Kayak Rentals</h4>
+                <ul>
+                  <li>First hour $70</li>
+                  <li>Two hours $100</li>
+                </ul>
+                <p>
+                  Group discount of 10% when you rent 5 or more kayaks - use
+                  Promo Code Summer2022 at check out! All first time-renters
+                  will receive 10 minute on-land orientation and paddling
+                  refresher before heading out on the water.
+                </p>
+              </div>
+            </CardItem>
+            <CardItem
+              src="images/pass.png"
+              text="Kayak Rental Passes"
+              // path="/products"
+            />
+            <CardItem
+              src="images/team.png"
+              text="Kayak Lessons"
+              // path="/sign-up"
+            />
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
