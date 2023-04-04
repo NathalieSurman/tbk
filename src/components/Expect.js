3e8cf3be56a9b25a316f09c7b8d88757a91f276c
Nathalie Surman <onepiecenat@hotmail.com>
2023-04-04 12:10:41 -0400

    Added a expect.js component and edited policy styles

---
+++ src/components/Expect.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import "./Expect.css"
+
+export const Expect = () => {
+  return (
+    <section class="articles-container">
+        {/* <h1>What to Expect</h1> */}
+  <article>
+    <div class="article-wrapper">
+      <figure>
+        <img src="./images/IMG-13.jpg" alt="sunset on the lake" />
+      </figure>
+      <div class="article-body">
+        <h2>Basic Requirements</h2>
+        <ul className='list-article'>
+            <li>
+            All first-time renters will be assessed for paddling abilities. 
+            Renters will need to demonstrate boat stability & control (e.g. going in a straight line, turning & stopping).
+            </li>
+            <li>
+            All renters 18 years of age or older must sign a Waiver (one per season)
+            </li>
+            <li>
+            Renters must be 18 years of age to sign a Waiver. Minors must have Waiver signed by a parent/guardian.
+            </li>
+            <li>Complete the on-line waiver BEFORE you come to the beach to eliminate unnecessary delays! </li>
+        </ul>
+    
+    </div>
+    </div>
+  </article>
+  <article>
+
+    <div class="article-wrapper">
+        <figure>
+            <img src="./images/IMG_1974.jpg" alt="" />
+        </figure>
+        <div class="article-body">
+        <h2>What to Wear, What to Bring</h2>
+        <ul className='list-article'>
+            <h4>Wear athletic attire: </h4>
+            <li>
+            Clothing that lets you move or simply a bathing suit.
+            </li>
+            <li>
+            Bottoms you don't mind getting wet and/or dry quickly.
+            </li>
+            <li>
+            Water sandals (most people go barefoot).
+            </li>
+            
+        </ul>
+        <ul className='list-article'>
+            <h4>Bring: </h4>
+            <li>
+            Hat
+            </li>
+            <li>
+            Sunscreen
+            </li>
+            <li>
+            Sunglasses
+            </li>
+            <li>
+            Ziplock or dry bag to protect phone & valuables when on the water.
+            </li>
+            <li>
+            Though we provide all safety equipment, please feel free to bring your own lifejacket.
+            </li>
+            <li>We are pet-friendly but do not provide life-jackets for pets. </li>
+        </ul>
+    
+        </div>
+    </div>
+    </article>
+    <article>
+
+    <div class="article-wrapper">
+    <figure>
+        <img src="./images/IMG_2012.jpg" alt="people on kayaks" />
+    </figure>
+    <div class="article-body">
+        <h2>First time paddling with Us?</h2>
+        <ul className='list-article'>
+            <li>
+            Remember traffic and parking can be difficult so plan your trip to us accordingly.
+            </li>
+            <li>
+            We are operating on a beach - your kayaks will be ready for you to launch.
+            </li>
+            <li>
+            We have a storage shed where you may leave your belongings when you are out on the water 
+            (we do not have secure lockers so leave valuables behind).
+            </li>
+            <li>You will be provided on-land orientation and basic refresher paddling instruction.</li>
+            <li>You will need to demonstrate some key skills including boat stability,
+                control, paddling a straight line, turning and stopping before heading out on the lake. </li>
+        </ul>
+    
+        </div>
+    </div>
+    </article>
+</section>
+)
+}
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { HeroSection } from '../HeroSection'
 import {Cards} from '../Cards'
-import '../../index.css'
 import { Policy } from '../Policy'
+import { Expect } from '../Expect'
 
 
 export const Home = () => {
@@ -11,6 +11,7 @@ export const Home = () => {
     <HeroSection />
     <Cards />
     <Policy/>
+    <Expect/>
   </>
   )
 }
