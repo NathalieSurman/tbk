660ec0072c73008553161f48e581db6b1c4751b4
Nathalie Surman <onepiecenat@hotmail.com>
2023-04-17 12:48:39 -0400

    changed the FAQ, made small changes everywhere

--- src/components/Policy.js
+++ src/components/Policy.js
@@ -8,10 +8,10 @@ export const Policy = () => {
     
     <h1 className='policy-title'>Policy</h1>
     
-    <div class="faq-drawer">
-      <input class="faq-drawer__trigger" id="faq-drawer" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer">PETS </label>
-      <div class="faq-drawer__content-wrapper">
-        <div class="faq-drawer__content">
+    <div class="policy-drawer">
+      <input class="policy-drawer__trigger" id="policy-drawer" type="checkbox" /><label class="policy-drawer__title" for="policy-drawer">PETS </label>
+      <div class="policy-drawer__content-wrapper">
+        <div class="policy-drawer__content">
           <p>
           We are pet friendly. Our regular (not small) single kayaks provide plenty of cockpit space for pet passengers.
     Please note that we do not provide life jackets for your furry friends.. 
@@ -20,10 +20,10 @@ export const Policy = () => {
       </div>
     </div>
     
-    <div class="faq-drawer">
-      <input class="faq-drawer__trigger" id="faq-drawer-2" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-2">CLOTHING</label>
-      <div class="faq-drawer__content-wrapper">
-        <div class="faq-drawer__content">
+    <div class="policy-drawer">
+      <input class="policy-drawer__trigger" id="policy-drawer-2" type="checkbox" /><label class="policy-drawer__title" for="policy-drawer-2">CLOTHING</label>
+      <div class="policy-drawer__content-wrapper">
+        <div class="policy-drawer__content">
           <p>
           For safety reasons we will not rent kayaks to persons wearing dresses/skirts/robes that fall below the knee. 
           </p>
@@ -31,10 +31,10 @@ export const Policy = () => {
       </div>
     </div>
     
-    <div class="faq-drawer">
-      <input class="faq-drawer__trigger" id="faq-drawer-3" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-3">CANCELLATION & WEATHER GUARANTEE</label>
-      <div class="faq-drawer__content-wrapper">
-        <div class="faq-drawer__content">
+    <div class="policy-drawer">
+      <input class="policy-drawer__trigger" id="policy-drawer-3" type="checkbox" /><label class="policy-drawer__title" for="policy-drawer-3">CANCELLATION & WEATHER GUARANTEE</label>
+      <div class="policy-drawer__content-wrapper">
+        <div class="policy-drawer__content">
           <p>
           All reservations cancelled with 24-hour notice and/or cancelled due to 
         unsafe or unpleasant paddling conditions will be rescheduled or fully refunded.           
@@ -43,10 +43,10 @@ export const Policy = () => {
       </div>
     </div>
 
-    <div class="faq-drawer">
-      <input class="faq-drawer__trigger" id="faq-drawer-4" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-4">AGE RESTRICTIONS</label>
-      <div class="faq-drawer__content-wrapper">
-        <div class="faq-drawer__content">
+    <div class="policy-drawer">
+      <input class="policy-drawer__trigger" id="policy-drawer-4" type="checkbox" /><label class="policy-drawer__title" for="policy-drawer-4">AGE RESTRICTIONS</label>
+      <div class="policy-drawer__content-wrapper">
+        <div class="policy-drawer__content">
           <p>
           Only persons 18 years and over can sign a waiver. Persons under 18 must have a waiver signed by a
     parent or guardian who is over 18 years of age. Paddlers under 18 years of age must be accompanied on the water 
@@ -56,46 +56,6 @@ export const Policy = () => {
         </div>
       </div>
     </div>
-    
   </div>
-
   )
 }
-
-
-
-{/* <TopDiv>
-<Title>Policies</Title>
-<Content>
-<FigureImg>
-<Img src="./images/dog.jpg" />
-</FigureImg>
-<Box>
-
-<Description>
-<div className='text'>
-    <h2>PETS</h2>
-    <p>We are pet friendly. Our regular (not small) single kayaks provide plenty of cockpit space for pet passengers.
-    Please note that we do not provide life jackets for your furry friends. </p>
-</div>
-<div className='text'>
-    <h2>CLOTHING</h2>
-    <p>For safety reasons we will not rent kayaks to persons wearing dresses/skirts/robes that fall below the knee. </p>
-</div>
-<div className='text'>
-    <h2>CANCELLATION & WEATHER GUARANTEE</h2>
-    <p>All reservations cancelled with 24-hour notice and/or cancelled due to 
-        unsafe or unpleasant paddling conditions will be rescheduled or fully refunded.</p>
-</div>
-<div className='text'>
-    <h2>AGE RESTRICTIONS</h2>
-    <p>Only persons 18 years and over can sign a waiver. Persons under 18 must have a waiver signed by a
-    parent or guardian who is over 18 years of age. Paddlers under 18 years of age must be accompanied on the water 
-    by an adult paddler over 18 years of age. Experienced paddlers under 18 years of age MAY paddle in our restricted beach zone 
-    but only if they can demonstrate strong paddling skills and the water is calm and wind is low.. </p>
-</div>
-</Description>
-</Box>
-
-</Content>
-</TopDiv> */}
--- src/components/pages/Faq.js
+++ src/components/pages/Faq.js
@@ -4,38 +4,92 @@ import "./Faq.css"
 
 export const Faq = () => {
   return (
-    <div className='faq'>
-<div class="container-faq">
-  <h1 className='faq-title-top'>Frequently Asked Questions</h1>
-  <details>
-  <summary>Do I have to take a lesson before I kayak with you? </summary>
-  <div>
-  No, but paddlers with limited/no paddling experience will only be permitted on calm days, 
+      
+  <div class="container-faq">
+    
+  <h1 className='faq-title'>Frequently Asked Questions</h1>
+  
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer">Do I have to take a lesson before I kayak with you? </label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        No, but paddlers with limited/no paddling experience will only be permitted on calm days, 
   will be given 10-15 minutes on-land instruction and must demonstrate key skills in our bay before heading out for a paddle.
+        </p>
+      </div>
+    </div>
   </div>
-</details>
-<details>
-  <summary>I don't have a reservation, can I just drop by for a kayak rental on the day?  </summary>
-  <div>
-  Of course you can. However, if you are a group of paddlers we may have a difficult time accommodating you on weekday evenings
+  
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer-2" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-2">I don't have a reservation, can I just drop by for a kayak rental on the day?</label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        Of course you can. However, if you are a group of paddlers we may have a difficult time accommodating you on weekday evenings
   and on weekends when we get busy so we strongly recommend making a reservation. 
+        </p>
+      </div>
+    </div>
   </div>
-</details>
-<details>
-  <summary>How old does a child have to be to paddle a kayak solo?</summary>
-  <div>
-  12 years of age if accompanied out on the water by an adult paddler. Ensure waiver for the child (under 18 years) 
-  is completed by Guardian. Children younger than 12 may participate by being a passenger on a kayak with experienced adult paddler.
+  
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer-3" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-3">How old does a child have to be to paddle a kayak solo?</label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        12 years of age if accompanied out on the water by an adult paddler. Ensure waiver for the child (under 18 years) 
+  is completed by Guardian. Children younger than 12 may participate by being a passenger on a kayak with experienced adult paddler.      
+        </p>
+      </div>
+    </div>
   </div>
-</details>
-<details>
-  <summary>What are your operating hours?</summary>
-  <div>
-  We are open every day that the weather and conditions are suitable for paddling from June 1 until the last week of September. 
+   
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer-4" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-4">What are your operating hours?</label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        We are open every day that the weather and conditions are suitable for paddling from June 1 until the last week of September.     
+        </p>
+      </div>
+    </div>
+  </div>
+   
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer-5" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-5">Can I bring my dog?</label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        Yes but the paddler must be experienced and must request a "Regular" kayak.
+    Our "Small" kayaks and "tandem" kayaks are not suitable for dog passengers.
+    Please also note that we do not supply dog life-jackets. Dogs do not need to sign a waiver 😉! 
+        </p>
+      </div>
+    </div>
+  </div>
+   
+  <div class="faq-drawer">
+    <input class="faq-drawer__trigger" id="faq-drawer-6" type="checkbox" /><label class="faq-drawer__title" for="faq-drawer-6">I have paddled with you before, do I need to sign a Waiver?</label>
+    <div class="faq-drawer__content-wrapper">
+      <div class="faq-drawer__content">
+        <p>
+        Every paddler needs to sign one waiver every season. One waiver per paddler per year!       
+        </p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+    
+  )
+}
 
-We are generally open from 9am until 30minutes before sunset (7:30-8:30pm) but we will open earlier for small groups. Just ask us!
-  </div>
-</details>
+
+{/* <div className='faq'>
+<div class="container-faq">
+  
+ 
 <details>
   <summary>Can I bring my dog?</summary>
   <div>
@@ -51,6 +105,4 @@ We are generally open from 9am until 30minutes before sunset (7:30-8:30pm) but w
   </div>
 </details>
 </div>
-    </div>
-  )
-}
+    </div> */}
