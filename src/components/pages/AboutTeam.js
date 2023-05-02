ee6335cf8379b22020cb305a2faa1ad233af4c97
Fennie Tang <fennie1996@gmail.com>
2023-05-01 20:30:17 -0400

    added info

--- src/components/pages/AboutTeam.js
+++ src/components/pages/AboutTeam.js
@@ -1,34 +1,49 @@
-import React from 'react'
-import "./AboutTeam.css"
+import React from "react";
+import "./AboutTeam.css";
 
 export const AboutTeam = () => {
-return (
-    <div className='container-team'>
-    <div class="main-div">
-  <h1 className='aboutteam-title'>Our Team</h1>
-  <ul class="cards-ul">
-    <li class="cards_item">
-        <div class="card">
-        <div class="card_image"><img src="/images/lousi.jpg" alt='' className='people-img'/></div>
-        <div class="card_content">
-            <h2 class="card_title">Louis</h2>
-            <p class="card_text">Owner</p>
-            <p class="card_text">Some text that describes me lorem ipsum ipsum lorem.</p>
-        
-        </div>
-        </div>
-    </li>
-    <li class="cards_item">
-        <div class="card">
-        <div class="card_image"><img src="/images/Moran.jpg" alt='' className='people-img'/></div>
-        <div class="card_content">
-        <h2 class="card_title">Moran</h2>
-            <p class="card_text">Instructor</p>
-            <p class="card_text">Some text that describes me lorem ipsum ipsum lorem.</p>
-        </div>
-      </div>
-    </li>
-    {/* <li class="cards_item">
+  return (
+    <div className="container-team">
+      <div class="main-div">
+        <h1 className="aboutteam-title">Our Team</h1>
+        <ul class="cards-ul">
+          <li class="cards_item">
+            <div class="card">
+              <div class="card_image">
+                <img src="/images/lousi.jpg" alt="" className="people-img" />
+              </div>
+              <div class="card_content">
+                <h2 class="card_title">Louis</h2>
+                <p class="card_text">Owner</p>
+                <p class="card_text">
+                  Louis is a student of environmental science and a talented
+                  athlete with strong paddling & climbing skills. Louis is the
+                  king of customer service and loves nothing more than helping
+                  customers get the most out of their paddling experience. He
+                  even gives great customer service in french!
+                </p>
+              </div>
+            </div>
+          </li>
+          <li class="cards_item">
+            <div class="card">
+              <div class="card_image">
+                <img src="/images/Moran.jpg" alt="" className="people-img" />
+              </div>
+              <div class="card_content">
+                <h2 class="card_title">Moran</h2>
+                <p class="card_text">Instructor</p>
+                <p class="card_text">
+                  Moran is a former competitive swimmer, lifeguard and swim
+                  instructor and has been kayaking as long as she can remember.
+                  Currently she is enrolled in chiropractic college. During the
+                  summer you may catch her swimming along our shoreline or on
+                  the water instructing.
+                </p>
+              </div>
+            </div>
+          </li>
+          {/* <li class="cards_item">
       <div class="card">
         <div class="card_image"><img src="https://picsum.photos/500/300/?image=11" alt='' className='people-img'/></div>
         <div class="card_content">
@@ -38,10 +53,8 @@ return (
         </div>
         </div>
     </li> */}
-
-  </ul>
-</div>
-</div>
-
-  )
-}
+        </ul>
+      </div>
+    </div>
+  );
+};
